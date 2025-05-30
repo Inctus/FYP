22d34084d5520638b2ea73e9166b7531af36c76b
Haashim-Ali Hussain <hh2721@ic.ac.uk>
2025-05-30 08:32:58 +0100

    Add experiment base

---
+++ src/experiment/experiment.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass
+class ExperimentParams:
+    pass
+
+class Experiment:
+    def __init__(self, params: ExperimentParams):
+        pass
+
+    def _search_hyperparameters(self):
+        pass
+
+    def run():
+        pass
